7fa295e93fc864802cf6e75fd3f1fba36a85075c
viveklm <vivek.aswal@learningmate.com>
2017-12-19 14:43:30 +0530

    to check

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.set('view engine', 'html');
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
 
-app.use(session({ resave: false,
-	      saveUninitialized: false,
+app.use(session({ resave: true,
+	      saveUninitialized: true,
 	      secret: 'uwotm8' }));
 
 app.use(express.static(path.join(__dirname, 'public')));
